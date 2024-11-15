3433fb922bd8d3e881dae6d56ed5a59c824bd568
Mahedihasanimam <mdmehedihasen678@gmail.com>
2024-11-15 10:16:46 +0600

    secound commit

---
+++ src/routes/route.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout/MainLayout";
+import Home from "../pages/home/Home";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout/>,
+      children: [
+        {
+          path: "/",
+          element: <Home/>,
+        },
+        {
+          path: "/about",
+          element: <h1>About</h1>,
+        },
+        {
+          path: "/contact",
+          element: <h1>Contact</h1>,
+        },
+      ],
+    },
+
+
+    // atuthentication routes
+    {
+        path: "/auth/login",
+        element: <h1>Login</h1>,
+    },
+    {
+        path: "/register",
+        element: <h1>Register</h1>,
+    }
+  ]);
